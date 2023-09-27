6ad40a912ecd3ae9663952d1f075d3b85d036347
samueltexa <woolardsamuels@gmail.com>
2023-09-27 19:52:42 +0300

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:order_wave/src/features/introduction/introduction1.dart';
+import 'package:order_wave/src/features/introduction/introduction.dart';
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Future.delayed(const Duration(seconds: 3));
-  FlutterNativeSplash.remove();
-  runApp(const MyApp());
+void main() {
+
+    runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'OrderWave',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Introduction1(),
+      home: const SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _removeSplash();
+  }
+
+  Future<void> _removeSplash() async {
+    await Future.delayed(const Duration(seconds: 2));
+    FlutterNativeSplash.remove();
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (_) => const IntroductionPage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // You can add your splash screen UI here if needed
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
     );
   }
 }
